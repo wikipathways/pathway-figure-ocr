6b3bab0d5732992a0b88eb992a956d8620b083c0
Alexander Pico <alex.pico@gladstone.ucsf.edu>
2018-01-20 12:24:33 -0800

    Minor updates based on new table names

--- postprocess.py
+++ postprocess.py
@@ -9,11 +9,11 @@ from normalize import normalize
 def postprocess(run_id, figure_id):
     conn = psycopg2.connect("dbname=pfocr")
     words_cur = conn.cursor()
-    runs_figures_words_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    for text_annotation in gcv_response['textAnnotations'][1:]:
+    ocr_processors__figures__words_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    for text_annotation in ocr_processors__figures['textAnnotations'][1:]:
         normalized_word = normalize(text_annotation['description'])
         if normalized_word:
             # This might not be the best way to insert. TODO: look at the proper way to handle this.
             words_cur.execute("INSERT INTO words (word) VALUES (%s) ON CONFLICT (word) DO UPDATE SET word = EXCLUDED.word RETURNING id;", (normalized_word, ))
             word_id = words_cur.fetchone()[0]
-            runs_figures_words_cur.execute("INSERT INTO runs_figures_words (run_id, figure_id, word_id) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING;", (run_id, figure_id, word_id))
+            ocr_processors__figures__words_cur.execute("INSERT INTO ocr_processors__figures__words (ocr_processor_id, figure_id, word_id) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING;", (run_id, figure_id, word_id))
