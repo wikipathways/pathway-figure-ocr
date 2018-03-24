8e4ecff214e6308c22f80a041d4d85100a55b80a
Alex Pico <apico@wikipathways-workspace.gladstone.internal>
2018-03-23 19:15:42 -0700

    added to stop list based on r1-check

--- transforms/stop.py
+++ transforms/stop.py
@@ -1,7 +1,7 @@
 from . import alphanumeric
 
 stop_list = ["2", "CO2", "HR", "PH", "CA2", "PR", "OF", "CO", "TYPE", "IN", "T3", "FA", "GR", "NP", "RD", "ON", "GA",
-        "DAMAGE", "GK", "CR", "DS", "ET", "JN", "S21", "S9", "PI", "PP", "TAT", "P"]
+        "DAMAGE", "GK", "CR", "DS", "ET", "JN", "S21", "S9", "PI", "PP", "TAT", "P", "GO", "L10"]
 
 def stop(word):
     alphanumerics = alphanumeric.alphanumeric(word)
