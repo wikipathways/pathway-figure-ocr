0621afa2ee681ba8bddbb45a5041a9ae5c05774c
Alex Pico <apico@wikipathways-workspace.gladstone.internal>
2018-03-23 19:08:55 -0700

    updated transforms based on r1-check

--- run.sh
+++ run.sh
@@ -8,18 +8,18 @@ function finish {
 trap 'finish $LINENO' SIGINT SIGTERM ERR
 
 ./pfocr.py clear;
-#./pfocr.py match -n stop -n nfkc -n upper -n ALPHA_to_A -n deburr -n alphanumeric -m root -m one_to_I;
-#./pfocr.py match -n stop -n nfkc -n upper -m root -n ALPHA_to_A -n deburr -n alphanumeric -m one_to_I;
-#./pfocr.py match -n stop -n nfkc -n upper -n ALPHA_to_A -n deburr -n alphanumeric -m one_to_I -m root;
+#./pfocr.py match -n stop -n nfkc -n upper -n swaps -n deburr -n alphanumeric -m root -m one_to_I;
+#./pfocr.py match -n stop -n nfkc -n upper -m root -n swaps -n deburr -n alphanumeric -m one_to_I;
+#./pfocr.py match -n stop -n nfkc -n upper -n swaps -n deburr -n alphanumeric -m one_to_I -m root;
 #./pfocr.py match -n stop -n nfkc -m expand -n stop -n nfkc -n upper;
 #./pfocr.py match -n stop -n nfkc -m expand -n stop -n nfkc -n deburr -n upper;
-#./pfocr.py match -n stop -n nfkc -m expand -n stop -n nfkc -n upper -n ALPHA_to_A -n deburr -n alphanumeric -m root -m one_to_I;
+#./pfocr.py match -n stop -n nfkc -m expand -n stop -n nfkc -n upper -n swaps -n deburr -n alphanumeric -m root -m one_to_I;
 
 # Note: upper messes with alphanumeric sometimes, e.g. NF-KB1
 ./pfocr.py match -n stop -m expand -n stop -m root -n alphanumeric;
-# Note: need a pass without root to avoid missing things like Ras
-./pfocr.py match -n stop -m expand -n stop -n upper -n ALPHA_to_A -n alphanumeric -m one_to_I;
-./pfocr.py match -n stop -n nfkc -n deburr -m expand -n stop -m root -n upper -n ALPHA_to_A -n alphanumeric -m one_to_I;
+# Note: need a pass though to upper without root to avoid missing things like Ras
+./pfocr.py match -n stop -m expand -n stop -n upper -n swaps -n alphanumeric ;
+./pfocr.py match -n stop -n nfkc -n deburr -m expand -n stop -m root -n upper -n swaps -n alphanumeric;
 
 ./pfocr.py summarize
 head -n 1 ./results.tsv > ./sample.tsv
--- transforms/swaps.py
+++ transforms/swaps.py
@@ -1,9 +1,17 @@
-swaps = {'ALPHA':'A','BETA':'B', 'GAMMA':'G', 'DELTA':'D', 'EPSILON':'E'}
+swaps = {
+'ALPHA':'A',
+'BETA':'B', 
+'GAMMA':'G', 
+'DELTA':'D', 
+'EPSILON':'E',
+'P13K':'PI3K',
+'EB13':'EBI3'
+}
 
 def multipleReplace(text, wordDict):
     for key in wordDict:
         text = text.replace(key, wordDict[key])
     return text
 
-def ALPHA_to_A(word):
+def swaps(word):
     return [multipleReplace(word, swaps)]
