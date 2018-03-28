fcbe4c198015c1c2c323513f53dcc7184de9f690
Alex Pico <apico@wikipathways-workspace.gladstone.internal>
2018-03-28 11:23:22 -0700

    r2 changes

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -1,11 +1,10 @@
-from . import ALPHA_to_A
 from . import alphanumeric
 from . import deburr
 from . import expand
-from . import one_to_I
+from . import swap
 from . import nfkc
 from . import noop
 from . import root
 from . import stop
 from . import upper
-__all__ = ["ALPHA_to_A", "alphanumeric", "deburr", "expand", "one_to_I", "nfkc", "noop", "root", "stop", "upper"]
+__all__ = ["swap", "alphanumeric", "deburr", "expand", "nfkc", "noop", "root", "stop", "upper"]
--- transforms/stop.py
+++ transforms/stop.py
@@ -1,7 +1,8 @@
 from . import alphanumeric
 
 stop_list = ["2", "CO2", "HR", "PH", "CA2", "PR", "OF", "CO", "TYPE", "IN", "T3", "FA", "GR", "NP", "RD", "ON", "GA",
-        "DAMAGE", "GK", "CR", "DS", "ET", "JN", "S21", "S9", "PI", "PP", "TAT", "P", "GO", "L10"]
+        "DAMAGE", "GK", "CR", "DS", "ET", "JN", "S21", "S9", "PI", "PP", "TAT", "P", "GO", "L10","CYCLIN","II","LE",
+	"TC","TX","UP","CAMP","FOR","DAG","PIP","IP","T","FATE","DM","FI","CF","HE","PL","PA","P8","S6","PM","ANG"]
 
 def stop(word):
     alphanumerics = alphanumeric.alphanumeric(word)
