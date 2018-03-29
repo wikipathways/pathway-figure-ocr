d7dc7e9744685088f04da8d061c7f0b37e2f8455
Alex Pico <apico@wikipathways-workspace.gladstone.internal>
2018-03-29 16:25:42 -0700

    a few more stops

--- transforms/stop.py
+++ transforms/stop.py
@@ -7,7 +7,8 @@ stop_list = ["2", "CO2", "HR", "PH", "CA2", "PR", "OF", "CO", "TYPE", "IN", "T3"
         "DAMAGE", "GK", "CR", "DS", "ET", "JN", "S21", "S9", "PI", "PP", "TAT", "GO", "L10","CYCLIN","II","LE",
 	"TC","TX","UP","CAMP","FOR","DAG","PIP","IP","FATE","DM","FI","CF","HE","PL","PA","P8","S6","PM","ANG","D1",
 	"AS","IS","OF","IN","AN","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V",
-	"W","X","Y","Z","NOT","NA","CAN","LI","AM","MIR","SS","ER","AR","T1"]
+	"W","X","Y","Z","NOT","NA","CAN","LI","AM","MIR","SS","ER","AR","T1","CEL","ECM","HITS","SR","AID","HDS","HK",
+	"REG"]
 
 
 def stop(word):
