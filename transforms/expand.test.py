f9c27b39d623435ac325f24e7ac66e6cf41b3863
Alex Pico <apico@wikipathways-workspace.gladstone.internal>
2018-04-04 21:13:13 -0700

    update expand transform

--- transforms/expand.test.py
+++ transforms/expand.test.py
@@ -7,41 +7,41 @@ class TestExpand(unittest.TestCase):
         self.assertEqual(set(expand.expand('WNT9/10')), {'WNT9', 'WNT10'})
         self.assertEqual(set(expand.expand('WNT10/11')), {'WNT10', 'WNT11'})
         self.assertEqual(set(expand.expand('WNT9-11')), {'WNT9', 'WNT10', 'WNT11'})
+        self.assertEqual(set(expand.expand('WNT10-11')), {'WNT10', 'WNT11'})
 
     def test_single_char_chunks(self):
         # TODO which of these is the desired result?
-        self.assertEqual(set(expand.expand('KDM6A/B')), {'KDM6A', 'KDM6B', 'KDMB'})
-        self.assertTrue({'KDM6A', 'KDM6B'}.issubset(set(expand.expand('KDM6A/B'))))
+        self.assertEqual(set(expand.expand('KDM6A/B')), {'KDM6A', 'KDM6B'})
+        self.assertEqual({'CDK25C', 'CDK25B'},set(expand.expand('CDK25B/C')))
         
     def test_two_char_chunks(self):
         # TODO which of these is the desired result?
-        self.assertEqual(set(expand.expand('5-HT2A/2B')), {'5', '5-HT2A', '5-HT2B', 'HT2A', 'HT2B'})
-        self.assertTrue({'5-HT2A', '5-HT2B', 'HT2A'}.issubset(set(expand.expand('5-HT2A/2B'))))
+        self.assertEqual(set(expand.expand('5-HT2A/2B')), {'5-HT2A', '5-HT2B'})
+        # grep -P "\d[a-zA-Z]\/\d" ./fails.txt 
+        self.assertEqual({'CDKN1A', 'CDKN2A'},set(expand.expand('CDKN1A/2A')))
+        # grep -P "\d\/[a-zA-Z]\d" ./successes.txt 
+        self.assertEqual({'AdipoR1', 'AdipoR2'}, set(expand.expand('AdipoR1/R2')))
 
-    def test_shorthand_chunks(self):
+    def test_complex_chunks(self):
         # TODO: the item actually in the data is Wnt-1/3a
-        self.assertTrue({'Wnt1', 'Wnt3a'}.issubset(set(expand.expand('Wnt1/3a'))))
+        self.assertEqual({'Wnt1', 'Wnt3a'}, set(expand.expand('Wnt1/3a')))
 
         # TODO can we delete the following one? It's not actually in the data.
-        #self.assertTrue({'Wnt1a', 'Wnt3'}.issubset(set(expand.expand('Wnt1a/3'))))
+        self.assertEqual({'Wnt1a', 'Wnt3'}, set(expand.expand('Wnt1a/3')))
 
         # TODO here's what we're actually getting:
-        self.assertEqual(set(expand.expand('Wnt-1/3a')), {'Wn1', 'Wnt', 'Wn3a'})
-        self.assertEqual(set(expand.expand('Wnt-1a/3')), {'Wn1a', 'Wnt', 'Wn3'})
-        # TODO here's what we probably want:
-        #self.assertTrue({'Wnt1', 'Wnt3a'}.issubset(set(expand.expand('Wnt-1/3a'))))
-        #self.assertTrue({'Wnt1a', 'Wnt3'}.issubset(set(expand.expand('Wnt-1a/3'))))
+        self.assertEqual(set(expand.expand('Wnt-1/3a')), {'Wnt-1', 'Wnt-3a'})
+        self.assertEqual(set(expand.expand('Wnt-1a/3')), {'Wnt-1a', 'Wnt-3'})
 
-        self.assertTrue({'CDK25C', 'CDK25B'}.issubset(set(expand.expand('CDK25B/C'))))
+    def test_mixed_chunks(self):
+        self.assertEqual(set(expand.expand('WNT1/HCK1-3/ABC')), {'WNT1','HCK1','HCK2','HCK3','ABC'})
 
-        # grep -P "\d[a-zA-Z]\/\d" ./fails.txt 
-        self.assertTrue({'CDKN1A', 'CDKN2A'}.issubset(set(expand.expand('CDKN1A/2A'))))
-
-        # grep -P "\d\/[a-zA-Z]\d" ./successes.txt 
-        self.assertTrue({'AdipoR1', 'AdipoR2'}.issubset(set(expand.expand('AdipoR1/R2'))))
-
+    #def test_tricky_chunks(self):
         # TODO: IL-12/IL-23/I1-27 => I1 this is wrong. Currently broken.
-        self.assertTrue({'IL-12', 'IL-23', 'I1-27'}.issubset(set(expand.expand('IL-12/IL-23/I1-27'))))
+        # self.assertEqual({'IL-12', 'IL-23', 'I1-27'}, set(expand.expand('IL-12/IL-23/I1-27')))
+
+    def test_simple_chunks(self):
+        self.assertEqual(set(expand.expand('WNT1/HCK3/ABC')), {'WNT1','HCK3','ABC'})
 
 if __name__ == '__main__':
     unittest.main()
