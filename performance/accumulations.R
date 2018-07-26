6dab29c9508d2116cebb021e9b4a43b0d796625b
Alexander Pico <apico@gladstone.ucsf.edu>
2018-07-25 18:00:21 -0700

    added performance scripts

---
+++ performance/accumulations.R
@@ -0,0 +1,54 @@
+setwd("~/Dropbox (Gladstone)/Pathway Figure OCR/wp_hs_pfocr")
+
+#read wp and 4000 pfocr
+pfocr.wp <- read.csv('20180418_wp_hs_pfocr_sub.csv',stringsAsFactors = F)
+pfocr.4000 <- read.csv('20180413_4000_pfocr_sub.csv',stringsAsFactors = F)
+
+#reshape pfocr into named list
+pfocr.wp.nl <- unstack(pfocr.wp[,2:1])
+pfocr.4000.nl <- unstack(pfocr.4000[,2:1])
+
+#average number of genes per pathway/image
+pfocr.wp.med<-median(unlist(lapply(pfocr.wp.nl, length)))
+pfocr.wp.med
+pfocr.4000.med<-median(unlist(lapply(pfocr.4000.nl, length)))
+pfocr.4000.med
+pfocr.wp.mean<-mean(unlist(lapply(pfocr.wp.nl, length)))
+pfocr.wp.mean
+pfocr.4000.mean<-mean(unlist(lapply(pfocr.4000.nl, length)))
+pfocr.4000.mean
+
+#plot accumulation of unique genes in pfocr WP
+unique.genes<-c()
+unique.genes.counts<-c()
+for (p in names(pfocr.nl)){
+    unique.genes <- c(unique.genes, setdiff(pfocr.nl[[p]],unique.genes))
+    unique.genes.counts <- c(unique.genes.counts,length(unique.genes))
+}
+plot(unique.genes.counts)
+## fit curve
+xx<-seq(1:length(unique.genes.counts))
+fit<-lm(unique.genes.counts~sqrt(xx))
+summary(fit)
+lines(xx, predict(fit, data.frame(x=xx)), col="red", lwd=2)
+predict(fit,data.frame(xx=269))  
+# 5300 WP pathways = 20,000 unique genes
+# 269 WP pathways = 4000 genes
+
+#plot accumulation of unique genes in pfocr 4000
+unique.4000.genes<-c()
+unique.4000.genes.counts<-c()
+for (p in names(pfocr.4000.nl)){
+    unique.4000.genes <- c(unique.4000.genes, setdiff(pfocr.4000.nl[[p]],unique.4000.genes))
+    unique.4000.genes.counts <- c(unique.4000.genes.counts,length(unique.4000.genes))
+}
+plot(unique.4000.genes.counts)
+## fit curve
+xx<-seq(1:length(unique.4000.genes.counts))
+fit<-lm(unique.4000.genes.counts~sqrt(xx))
+summary(fit)
+plot(unique.4000.genes.counts)
+lines(xx, predict(fit, data.frame(x=xx)), col="red", lwd=2)
+predict(fit,data.frame(xx=2925))  
+# 12,000 PFOCR images = 8000 unique genes
+# 2925 images = 4000 genes
---
+++ performance/heatmaps.R
@@ -0,0 +1,191 @@
+install.packages(c("gplots","ggplot2","RColorBrewer"))
+library(ggplot2,RColorBrewer,gplots)
+# Alt color palette to consider: https://cran.r-project.org/web/packages/viridis/index.html
+
+setwd("~/Dropbox (Gladstone)/Pathway Figure OCR/wp_hs_pfocr")
+
+#read pfocr 
+pfocr.4000 <- read.csv('20180413_4000_pfocr_sub.csv',stringsAsFactors = F)
+
+#reshape pfocr into named lists by genes
+## all results
+pfocr.4000.genes.nl <- unstack(pfocr.4000[,1:2])
+## subset excluding bioentities hits
+pfocr.4000.nobe.genes.nl <- unstack(pfocr.4000[sapply(pfocr.4000$source, function(x) x!="bioentities_symbol"),1:2])
+
+
+#HEATMAP #1: all results
+## first, prepare subsets to cluster and plot
+## only genes on more than x pmc...
+pfocr.4000.genes.count<-lapply(pfocr.4000.genes.nl,length)
+pfocr.4000.top.genes<-names(pfocr.4000.genes.count[sapply(pfocr.4000.genes.count, function(x) x>10)])
+pfocr.4000.top.genes.nl<-pfocr.4000.genes.nl[pfocr.4000.top.genes]
+### reshape into named list by pmc
+pfocr.4000.top.genes.sk<-stack(pfocr.4000.top.genes.nl)
+pfocr.4000.top.nl<-unstack(pfocr.4000.top.genes.sk[,2:1])
+
+## ...and only pmc with more than y of those genes
+pfocr.4000.top.count<-lapply(pfocr.4000.top.nl,length)
+pfocr.4000.top.pmc<-names(pfocr.4000.top.count[sapply(pfocr.4000.top.count, function(y) y>24)])
+pfocr.4000.top.nl<-pfocr.4000.top.nl[pfocr.4000.top.pmc]
+
+## get unique gene list for x-axis
+unique.4000.top.genes<-c()
+for (p in names(pfocr.4000.top.nl)){
+    unique.4000.top.genes <- c(unique.4000.top.genes, setdiff(pfocr.4000.top.nl[[p]],unique.4000.top.genes))
+}
+
+## initialize dataframe for heatmap
+pfocr.4000.top.matrix.df <- data.frame(matrix(nrow=length(names(pfocr.4000.top.nl)),ncol=length(unique.4000.top.genes)))
+row.names(pfocr.4000.top.matrix.df)<-names(pfocr.4000.top.nl)
+colnames(pfocr.4000.top.matrix.df)<-as.character(unique.4000.top.genes)
+
+## build dataframe 
+### WARNING: This takes a few minutes
+t=0
+for (p in names(pfocr.4000.top.nl)){
+    print(paste(p,t,sep=" -- "))
+    t=t+1
+    for (g in unique.4000.top.genes){
+        val<-0
+        if(g %in% pfocr.4000.top.nl[[p]]) val<-1
+        pfocr.4000.top.matrix.df[p,as.character(g)]<-val
+    }
+}
+
+## calc clusters
+d = dist(pfocr.4000.top.matrix.df, method = "binary")
+hc = hclust(d, method="ward.D2")
+
+## cut into k.num clusters and prep color strip
+k.num=20
+hh<-cutree(hc,k=k.num )
+clusters<-split(names(hh),hh)
+nofclust.height <-  length(unique(as.vector(hh)));
+selcol <- colorRampPalette(RColorBrewer::brewer.pal(9,"Set1"))
+clustcol.height = selcol(nofclust.height);
+
+## prep matrix, order and color by clusters
+pfocr.4000.top.matrix<- as.matrix(pfocr.4000.top.matrix.df)
+pfocr.4000.top.matrix.k<-as.matrix(pfocr.4000.top.matrix[unname(unlist(clusters)),])
+hh.k<-hh[unname(unlist(clusters))]
+#write.csv(hh.k,"clusters_top10x24.csv")
+
+## plot (uncomment to print)
+#png(filename = "pfocr_4000_top10x24_cluster.png", width = 8000, height=8800,res = 170)
+gplots::heatmap.2(pfocr.4000.top.matrix.k, 
+                  key = F,
+                  dendrogram="none",
+                  trace="none",
+                  scale="none", 
+                  col=c("white","blue"), 
+                  RowSideColors=clustcol.height[hh.k],
+                  xlab="Entrez Gene IDs",
+                  ylab="PMCIDs",
+                  Rowv=F
+)
+#dev.off()
+
+#HEATMAP #2: excluding bioentites hits
+## first, prepare subsets to cluster and plot
+## only genes on more than x pmc...
+pfocr.4000.nobe.genes.count<-lapply(pfocr.4000.nobe.genes.nl,length)
+pfocr.4000.nobe.top.genes<-names(pfocr.4000.nobe.genes.count[sapply(pfocr.4000.nobe.genes.count, function(x) x>10)])
+pfocr.4000.nobe.top.genes.nl<-pfocr.4000.nobe.genes.nl[pfocr.4000.nobe.top.genes]
+### reshape into named list by pmc
+pfocr.4000.nobe.top.genes.sk<-stack(pfocr.4000.nobe.top.genes.nl)
+pfocr.4000.nobe.top.nl<-unstack(pfocr.4000.nobe.top.genes.sk[,2:1])
+
+## ...and only pmc with more than y of those genes
+pfocr.4000.nobe.top.count<-lapply(pfocr.4000.nobe.top.nl,length)
+pfocr.4000.nobe.top.pmc<-names(pfocr.4000.nobe.top.count[sapply(pfocr.4000.nobe.top.count, function(y) y>7)])
+pfocr.4000.nobe.top.nl<-pfocr.4000.nobe.top.nl[pfocr.4000.nobe.top.pmc]
+
+## get unique gene list for x-axis
+unique.4000.nobe.top.genes<-c()
+for (p in names(pfocr.4000.nobe.top.nl)){
+    unique.4000.nobe.top.genes <- c(unique.4000.nobe.top.genes, setdiff(pfocr.4000.nobe.top.nl[[p]],unique.4000.nobe.top.genes))
+}
+
+## initialize dataframe for heatmap
+pfocr.4000.nobe.top.matrix.df <- data.frame(matrix(nrow=length(names(pfocr.4000.nobe.top.nl)),ncol=length(unique.4000.nobe.top.genes)))
+row.names(pfocr.4000.nobe.top.matrix.df)<-names(pfocr.4000.nobe.top.nl)
+colnames(pfocr.4000.nobe.top.matrix.df)<-as.character(unique.4000.nobe.top.genes)
+
+## build dataframe 
+### WARNING: This takes a few minutes
+t=0
+for (p in names(pfocr.4000.nobe.top.nl)){
+    print(paste(p,t,sep=" -- "))
+    t=t+1
+    for (g in unique.4000.nobe.top.genes){
+        val<-0
+        if(g %in% pfocr.4000.nobe.top.nl[[p]]) val<-1
+        pfocr.4000.nobe.top.matrix.df[p,as.character(g)]<-val
+    }
+}
+
+## calc clusters
+d2 = dist(pfocr.4000.nobe.top.matrix.df, method = "binary")
+hc2 = hclust(d2, method="ward.D2")
+
+## cut into k.num clusters and prep color strip
+k.num=20
+hh2<-cutree(hc2,k=k.num )
+clusters2<-split(names(hh2),hh2)
+nofclust.height2 <-  length(unique(as.vector(hh2)));
+selcol2 <- colorRampPalette(RColorBrewer::brewer.pal(9,"Set1"))
+clustcol.height2 = selcol2(nofclust.height2);
+
+## prep matrix, order and color by clusters
+pfocr.4000.nobe.top.matrix<- as.matrix(pfocr.4000.nobe.top.matrix.df)
+pfocr.4000.nobe.top.matrix.k<-as.matrix(pfocr.4000.nobe.top.matrix[unname(unlist(clusters2)),])
+hh.k2<-hh2[unname(unlist(clusters2))]
+#write.csv(hh.k2,"clusters_nobe_top10x7.csv")
+
+## plot (uncomment to print)
+#png(filename = "pfocr_4000_nobe_top10x7_cluster.png", width = 8000, height=10000,res = 175)
+gplots::heatmap.2(pfocr.4000.nobe.top.matrix.k, 
+                  key = F,
+                  dendrogram="none",
+                  trace="none",
+                  scale="none", 
+                  col=c("white","blue"), 
+                  RowSideColors=clustcol.height2[hh.k2],
+                  xlab="Entrez Gene IDs",
+                  ylab="PMCIDs",
+                  Rowv=F
+)
+#dev.off()
+
+# #HEATMAP #2b: subset of #2 for figure
+# ## clusters 8-13
+# ## subset clusters and prep color strip
+# sub.k.num.start=8
+# sub.k.num.end=13
+# sub.k.num=sub.k.num.end-sub.k.num.start+1
+# clusters2b<-clusters2[8:13]
+# selcol2b <- colorRampPalette(RColorBrewer::brewer.pal(sub.k.num,"Set1"))
+# clustcol.height2b = selcol2b(sub.k.num);
+# 
+# ## prep matrix, order and color by clusters
+# pfocr.4000.nobe.top.matrix2b<- as.matrix(pfocr.4000.nobe.top.matrix.df)
+# pfocr.4000.nobe.top.matrix.k2b<-as.matrix(pfocr.4000.nobe.top.matrix2b[unname(unlist(clusters2b)),])
+# hh.k2b<-hh2[unname(unlist(clusters2b))]
+# hh.k2b<-sapply(hh.k2b, function(x) x-(sub.k.num.start-1))
+# #write.csv(hh.k2b,"clusters_nobe_top10x7sub.csv")
+# 
+# ## plot (uncomment to print)
+# #png(filename = "pfocr_4000_nobe_top10x7sub_cluster.png", width = 5000, height=6000,res = 175)
+# gplots::heatmap.2(pfocr.4000.nobe.top.matrix.k2b, 
+#                   key = F,
+#                   dendrogram="none",
+#                   trace="none",
+#                   scale="none", 
+#                   col=c("white","blue"), 
+#                   RowSideColors=clustcol.height2b[hh.k2b],
+#                   xlab="Entrez Gene IDs",
+#                   ylab="PMCIDs",
+#                   Rowv=F
+# )
+# #dev.off()
