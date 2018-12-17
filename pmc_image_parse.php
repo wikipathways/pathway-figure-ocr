36ab34a1a00cdbec08aa25c188f98c901fd3636b
Alexander Pico <apico@gladstone.ucsf.edu>
2018-12-16 21:25:08 -0800

    simplified image parse

--- pmc_image_parse.php
+++ pmc_image_parse.php
@@ -1,25 +1,15 @@
 <?php
-include 'simple_html_dom.php';
 
-$htmldir = 'pmc/signaling_pathway/1.1_rawhtml/'; 
-$outdir = 'pmc/signaling_pathway/1.1.1_figures/';
+$htmldir = '20181216/rawhtml/'; 
+$outdir = '20181216/figures/';
 $ncbi = "http://www.ncbi.nlm.nih.gov";
 
 foreach(glob($htmldir.'*.html') as $fn){
-	$dom = new DOMDocument;
-	$html = file_get_html($fn);
-	libxml_use_internal_errors(true);
-	$dom->loadHTML($html);
-	libxml_use_internal_errors(false);
-
-	$results = $html->find('div.rslt'); //each result is encapsulated by div.rlst
-	foreach($results as $rslt){
-		
-		$caption = $rslt->find('div.rprt_cont p.details');
-
-		//Process image tag from result and write
-		$image = $rslt->find('img');
-                $name = $image[0]->getAttribute('src-large');
+	echo $fn, "\n";
+	$stuff = file_get_contents($fn);
+	preg_match_all('/src-large\=\"(.*?)\"/', $stuff, $imgurl);
+	foreach($imgurl[1] as $name){
+		echo $name, "\n";
                 preg_match('/instance\/(\d+)\/bin\/(.*)$/', $name, $matches);
 		$pmcid = "PMC" . $matches[1];
 		$originalFilename = $matches[2];
@@ -27,20 +17,7 @@ foreach(glob($htmldir.'*.html') as $fn){
                 $url = $ncbi . $name;
                 $imageContent = file_get_contents($url);
                 file_put_contents($imageFilename, $imageContent);
-
-		//Process caption-related tags from result and write
-		$title = $rslt->find('div.rprt_cont p.title');
-                $titleTxt = $title[0]->innertext;
-		$captionTxt = '<div class="p4l_caption"><div class="p4l_captionTitle">';
-		$captionTxt .= str_replace ('href="/pmc','target="_blank" href="' . $ncbi . '/pmc', $titleTxt);
-		$captionTxt .= '</div><br /><div class="p4l_captionBody">';
-                $details = $rslt->find('div.rprt_cont p.details');
-                $detailsTxt = strip_tags($caption[0]->innertext, '<b>');
-                $captionTxt .= $detailsTxt . "</div></div>";
-		$captionFilename = $outdir . $pmcid . "__" . $originalFilename. '.html';
-		file_put_contents($captionFilename, $captionTxt);		
- 	}
-	
+	}
 }
 
 ?>
