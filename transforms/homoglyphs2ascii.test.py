d8e52a3746d37a640140ff669dff79c9777c313c
Anders Riutta <git@andersriutta.com>
2019-01-28 13:17:52 -0800

    Add db_copy method to pfocr.py. Improve transform tests.

--- transforms/homoglyphs2ascii.test.py
+++ transforms/homoglyphs2ascii.test.py
@@ -1,24 +1,18 @@
 import unittest
 import homoglyphs2ascii
 
-# TODO: find a better way to run the tests.
-# For now, this works:
-# cd transforms
-# python homoglyphs2ascii.test.py TestHomoglyphs.latin_and_cyrillic_are_homoglyphs
-# python homoglyphs2ascii.test.py TestHomoglyphs.to_ascii_makes_equal
-
 
 class TestHomoglyphs(unittest.TestCase):
 
-    #cyrillic = 'МАРК8'
-    #latin = 'MAPK8'
+    cyrillic = 'МАРК8'
+    latin = 'MAPK8'
 
-    def latin_and_cyrillic_are_homoglyphs(self):
-        self.assertNotEqual('МАРК8', 'MAPK8')
+    def test_latin_and_cyrillic_are_homoglyphs(self):
+        self.assertNotEqual(self.cyrillic, self.latin)
 
-    def to_ascii_makes_equal(self):
+    def test_to_ascii_makes_equal(self):
         self.assertEqual(homoglyphs2ascii.homoglyphs2ascii(
-            'МАРК8'), homoglyphs2ascii.homoglyphs2ascii('MAPK8'))
+            self.cyrillic), homoglyphs2ascii.homoglyphs2ascii(self.latin))
 
 if __name__ == '__main__':
     unittest.main()
