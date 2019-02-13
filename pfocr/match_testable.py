b270f26c721e8e688b366497cb8c9edeb1af68a5
Anders Riutta <git@andersriutta.com>
2019-02-13 12:08:45 -0800

    Better tracking of match history

--- pfocr/match_testable.py
+++ pfocr/match_testable.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import hashlib
 import json
-import psycopg2
-import psycopg2.extras
 import re
 import transforms
 import sys
-from deadline import deadline
-from get_pg_conn import get_pg_conn
 
+from deadline import deadline
 
 # see https://docs.python.org/3/library/re.html?highlight=re#writing-a-tokenizer
 
-
-alphanumeric_re = re.compile('\w')
-whitespace_re = re.compile('[ \t]')
+# we need re.DOTALL to handle cases like '\nABC1'
+has_alphanumeric_re = re.compile('.*\w.*', re.DOTALL)
+to_underscore_re = re.compile('[ \t]')
 
 # NOTE: all single letter symbols have already been removed from the lexicon
 # NOTE: all double letter symbols have already been removed from prev_symbol, alias_symbol; 
@@ -30,34 +26,55 @@ stop_words = {"2", "CO2", "HR", "GA", "CA2", "TYPE",
 
 # We run this normalization regardless of the -n and -m items specified
 def normalize_always(word):
-    return whitespace_re.sub('_', word.upper())
+    return to_underscore_re.sub('_', word.upper())
 
-# for the OCR full text from a single figure
-def match_one(transform_names_categories_functions, symbol_ids_by_symbol, text):
-    try:
-        matches = set()
-        transforms_applied = []
-        transformed_words = [text]
-        for transform_name_category_function in transform_names_categories_functions:
-            transforms_applied.append(transform_name_category_function["name"])
-            # TODO: re-enable this?
-            #for transformed_word_prev in [x for x in transformed_words if alphanumeric_re.match(x)]:
-            for transformed_word_prev in transformed_words:
-                transformed_words = []
-                # the symbols column never has spaces, but it does have underscores
-                # ABL_family
-                for transformed_word in transform_name_category_function["function"](transformed_word_prev):
-                    normalized_transformed_word = normalize_always(transformed_word)
-                    if normalized_transformed_word in symbol_ids_by_symbol: 
-                        matches.add(normalized_transformed_word)
-                    else:
-                        transformed_words.append(transformed_word)
+# Find match(es) from the OCR full text from a single figure
+def attempt_match(symbol_ids_by_symbol, transform_names_categories_functions, acc, text, history, transform_index=0):
 
-        return matches
+    # Does the most recent history item indicate we got a match?
+    last_history = history[len(history) - 1]
+    if ("matched" in last_history) and last_history["matched"]:
+        return acc
 
-    except(Exception) as e:
-        print('Unexpected Error in match_one:', e)
-        raise
+    if not has_alphanumeric_re.match(text):
+        acc.append({
+            "history": history,
+            "transform_index": transform_index,
+            "matched": False
+            })
+        return acc
+
+    normalized_transformed_word = normalize_always(text)
+    if normalized_transformed_word in symbol_ids_by_symbol: 
+        acc.append({
+            "history": history,
+            "transform_index": transform_index,
+            "matched": True,
+            "matched_text": normalized_transformed_word,
+            "symbol_id": symbol_ids_by_symbol[normalized_transformed_word]
+            })
+        return acc
+
+    if transform_index >= len(transform_names_categories_functions):
+        acc.append({
+            "history": history,
+            "transform_index": transform_index,
+            "matched": False
+            })
+        return acc
+
+    for transform_name_category_function in transform_names_categories_functions[transform_index:]:
+        transform_name = transform_name_category_function["name"]
+        transform_category = transform_name_category_function["category"]
+        transform_function = transform_name_category_function["function"]
+        transform_index += 1
+        for transformed_word in transform_function(text):
+            history_copy = history.copy()
+            history_copy.append({
+                "transform": transform_name,
+                "text": transformed_word,
+                })
+            attempt_match(symbol_ids_by_symbol, transform_names_categories_functions, acc, transformed_word, history_copy, transform_index)
 
 # texts is a list of full text strings from the OCR, one per figure.
 def match_multiple(transform_names_and_categories, texts, symbols_and_ids):
@@ -93,8 +110,12 @@ def match_multiple(transform_names_and_categories, texts, symbols_and_ids):
 
         all_matches = set()
         for text in texts:
-            for match in match_one(transform_names_categories_functions, symbol_ids_by_symbol, text):
-                all_matches.add(match)
+            acc = list()
+            attempt_match(symbol_ids_by_symbol, transform_names_categories_functions, acc, text, [{"transform": None, "text": text}])
+            for x in acc:
+                if x["matched"]:
+                    all_matches.add(x["matched_text"])
+            #print(json.dumps(acc, indent=2))
 
         return all_matches
 
