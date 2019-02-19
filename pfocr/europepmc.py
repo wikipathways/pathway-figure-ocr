dd17652ade7fe3bcdf0659c614ebdc944706281c
Anders Riutta <git@andersriutta.com>
2019-02-19 13:38:58 -0800

    Remove duplicates so symbols are unique by PMC ID.

--- pfocr/europepmc.py
+++ pfocr/europepmc.py
@@ -6,10 +6,13 @@
 
 # We are currently using the "sentence-based annotations" format
 
-# Here's a useful jq query to get all the annotation exact values:
+# Useful Commands:
+#
+# Save to file:
+# ./pfocr.py europepmc pfocr2018121717 > ../europepmc_wikipathways_20190219.json 
+#
+# Use jq query to get all the annotation exact values:
 # python europepmc.py | jq -s '.[] | .anns[] | .exact'
-# And one to get figure_filepath and position (but you must first uncomment figure_filepath in the code):
-# python europepmc.py | jq -s '.[] | .anns[] | {exact, figure_filepath}'
 
 # TODO: run the output through this
 # https://github.com/EuropePMC/EuropePMC-Annotation-Validator
@@ -46,16 +49,15 @@ def europepmc(args):
         #parsedhtml = json.loads(open(Path(PurePath(CURRENT_SCRIPT_PATH, "parsedhtml.json")), "r").read())
 
         annotations_query = '''
-        SELECT pmcid, figure_filepath, word, symbol, source, hgnc_symbol, xref as entrez
+        SELECT DISTINCT pmcid, symbol, hgnc_symbol, xref as entrez
         FROM figures__xrefs
-        ORDER BY pmcid, figure_filepath, word;
+        ORDER BY pmcid, symbol;
         '''
         annotations_cur.execute(annotations_query)
 
         annotations_by_pmcid = {}
         for row in annotations_cur:
             pmcid = row["pmcid"]
-            figure_filepath = row["figure_filepath"]
 
             if pmcid not in annotations_by_pmcid:
                 annotations_by_pmcid[pmcid] = {
@@ -65,58 +67,14 @@ def europepmc(args):
                     "anns": []
                     }
 
-#            # NOTE: the code below still can't find a figure id for
-#            # a few results, such as
-#            # /home/pfocr/pmc/20181216/images/PMC5768180__fimmu-08-01938-g006b.jpg
-#            stem = (PurePath(figure_filepath).stem)
-#            pmc_figure_hashlike = stem.split('__')[1]
-#            figure_id_candidates = []
-#            if pmc_figure_hashlike in parsedhtml:
-#                parsedhtml_data = parsedhtml[pmc_figure_hashlike]
-#                figure_id_candidates.append(parsedhtml_data.get("figure_id"))
-#                figure_id_candidates.append(parsedhtml_data.get("figure_id_from_image_link"))
-#                figure_id_candidates.append(parsedhtml_data.get("figure_id_from_text"))
-#            else:
-#                parsedhtml_data_candidates = []
-#                for v in parsedhtml.values():
-#                    if v["pmcid"] == pmcid:
-#                        parsedhtml_data_candidates.append(v)
-#                parsedhtml_data = None
-#                if len(parsedhtml_data_candidates) == 1:
-#                    parsedhtml_data = parsedhtml_data_candidates[0]
-#                    figure_id_candidates.append(parsedhtml_data.get("figure_id"))
-#                    figure_id_candidates.append(parsedhtml_data.get("figure_id_from_image_link"))
-#                    figure_id_candidates.append(parsedhtml_data.get("figure_id_from_text"))
-#
-#            if fig_id_re.match(stem):
-#                figure_id_candidates.append(int(fig_id_re.search(stem).group(1)))
-#
-#            figure_id = next((item for item in figure_id_candidates if item is not None), None)
-#            #figure_id = next(item for item in figure_id_candidates if item is not None)
-#            
-#            print(figure_id_candidates)
-#            if not figure_id:
-#                print(figure_filepath)
-#                if pmc_figure_hashlike in parsedhtml:
-#                    parsedhtml_data = parsedhtml[pmc_figure_hashlike]
-#                    print(parsedhtml_data)
-#                raise Exception('Failed to get figure_id for the data above')
-
-            word = row["word"]
             symbol = row["symbol"]
-            source = row["source"]
             hgnc_symbol = row["hgnc_symbol"]
             entrez = row["entrez"]
-            # NOTE: We could extract a crop of the figure corresponding to
-            # bounding box of the matched text and base64-encode it.
-            #"figure": os.path.basename(figure_filepath),
 
             # TODO: don't add the same value again if it has the same entrez
             # for a given pmcid
             annotations_by_pmcid[pmcid]["anns"].append(
                     {
-                        # figure_filepath is just for our internal dev use.
-                        #"figure_filepath": figure_filepath,
                         "exact": symbol,
                         "section": "Figure",
                         "tags": [
