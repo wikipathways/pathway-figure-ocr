5167c1a99ce472ba6007d03630d66642f4e8afe9
Alexander Pico <apico@gladstone.ucsf.edu>
2023-05-23 10:52:03 -0700

    Add common false positive NFE

--- transforms/stop.py
+++ transforms/stop.py
@@ -10,7 +10,7 @@ stop_list = ["2", "CO2", "HR", "GA", "CA2", "TYPE",
         "DAMAGE", "GK", "S21", "TAT", "L10","CYCLIN",
 	"CAMP","FOR","DAG","PIP","FATE","ANG",
 	"NOT","CAN","MIR","CEL","ECM","HITS","AID","HDS",
-	"REG","ROS", "D1", "CALL", "BEND3"]
+	"REG","ROS", "D1", "CALL", "BEND3", "NFE"]
 
 
 def stop(word):
